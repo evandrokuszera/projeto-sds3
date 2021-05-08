6d9c1a19c475f59950ead9106c98674bb45b74b8
Evandro Kuszera <evandrokuszera@gmail.com>
2021-05-08 10:53:05 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { SSL_OP_NO_TLSv1_1 } from "constants";
+
+function Home() {
+    return (
+        <div>
+            <h1>Página Home</h1>
+        </div>
+    );
+}
+
+export default Home;
