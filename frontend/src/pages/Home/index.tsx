c33e39a262005b764839c22518bc83f90a2a2ee5
Evandro Kuszera <evandrokuszera@gmail.com>
2021-05-09 21:26:40 -0300

    Env var - correção 1

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,5 @@
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
-import { SSL_OP_NO_TLSv1_1 } from "constants";
 import { Link } from "react-router-dom";
 
 function Home() {
